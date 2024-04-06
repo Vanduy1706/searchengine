ac90d1e195d1be3ab2887af6cd956f84a1f1ac18
Vo Van Duy <vovanduy170603@gmail.com>
2024-04-07 00:32:20 +0700

    Back end here

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const { webData } = require('./webData');
+
+const app = express();
+app.use(express.static('./public'))
+
+const hostName = 'localhost';
+const port = 3000;
+
+app.get('/website/data', (req, res) => {
+  res.json(webData);
+})
+
+app.get('*', (req, res) => {
+  res.status(404).send('resource not found');
+})
+
+app.listen(port, hostName, () => {
+  console.log(`Server running at http://${hostName}:${port}`);
+});
---
+++ webData.js
@@ -0,0 +1,505 @@
+const webData = [
+    {
+        "id": "1",
+        "name": "youtube",
+        "url": "https://youtube.com"
+    },
+    {
+        "id": "2",
+        "name": "facebook",
+        "url": "https://facebook.com"
+    },
+    {
+        "id": "3",
+        "name": "twitter",
+        "url": "https://twitter.com"
+    },
+    {
+        "id": "4",
+        "name": "google",
+        "url": "https://google.com"
+    },
+    {
+        "id": "5",
+        "name": "y8",
+        "url": "https://y8.com"
+    },
+    {
+        "id": "6",
+        "name": "amazon",
+        "url": "https://amazon.com"
+    },
+    {
+        "id": "7",
+        "name": "stackoverflow",
+        "url": "https://stackoverflow.com"
+    },
+    {
+        "id": "8",
+        "name": "github",
+        "url": "https://github.com"
+    },
+    {
+        "id": "9",
+        "name": "wikipedia",
+        "url": "https://wikipedia.org"
+    },
+    {
+        "id": "10",
+        "name": "linkedin",
+        "url": "https://linkedin.com"
+    },
+    {
+        "id": "11",
+        "name": "apple",
+        "url": "https://apple.com"
+    },
+    {
+        "id": "12",
+        "name": "microsoft",
+        "url": "https://microsoft.com"
+    },
+    {
+        "id": "13",
+        "name": "netflix",
+        "url": "https://netflix.com"
+    },
+    {
+        "id": "14",
+        "name": "instagram",
+        "url": "https://instagram.com"
+    },
+    {
+        "id": "15",
+        "name": "ebay",
+        "url": "https://ebay.com"
+    },
+    {
+        "id": "16",
+        "name": "walmart",
+        "url": "https://walmart.com"
+    },
+    {
+        "id": "17",
+        "name": "pinterest",
+        "url": "https://pinterest.com"
+    },
+    {
+        "id": "18",
+        "name": "craigslist",
+        "url": "https://craigslist.org"
+    },
+    {
+        "id": "19",
+        "name": "imdb",
+        "url": "https://imdb.com"
+    },
+    {
+        "id": "20",
+        "name": "bing",
+        "url": "https://bing.com"
+    },
+    {
+        "id": "21",
+        "name": "paypal",
+        "url": "https://paypal.com"
+    },
+    {
+        "id": "22",
+        "name": "twitch",
+        "url": "https://twitch.tv"
+    },
+    {
+        "id": "23",
+        "name": "dropbox",
+        "url": "https://dropbox.com"
+    },
+    {
+        "id": "24",
+        "name": "spotify",
+        "url": "https://spotify.com"
+    },
+    {
+        "id": "25",
+        "name": "adobe",
+        "url": "https://adobe.com"
+    },
+    {
+        "id": "26",
+        "name": "cuutruyen",
+        "url": "https://cuutruyen.com"
+    },
+    {
+        "id": "27",
+        "name": "cnn",
+        "url": "https://cnn.com"
+    },
+    {
+        "id": "28",
+        "name": "tuổi trẻ",
+        "url": "https://tuoitre.vn"
+    },
+    {
+        "id": "29",
+        "name": "hulu",
+        "url": "https://hulu.com"
+    },
+    {
+        "id": "30",
+        "name": "aliexpress",
+        "url": "https://aliexpress.com"
+    },
+    {
+        "id": "31",
+        "name": "stackexchange",
+        "url": "https://stackexchange.com"
+    },
+    {
+        "id": "32",
+        "name": "pornhub",
+        "url": "https://pornhub.com"
+    },
+    {
+        "id": "33",
+        "name": "wordpress",
+        "url": "https://wordpress.com"
+    },
+    {
+        "id": "34",
+        "name": "espn",
+        "url": "https://espn.com"
+    },
+    {
+        "id": "35",
+        "name": "nfl",
+        "url": "https://nfl.com"
+    },
+    {
+        "id": "36",
+        "name": "nba",
+        "url": "https://nba.com"
+    },
+    {
+        "id": "37",
+        "name": "nhl",
+        "url": "https://nhl.com"
+    },
+    {
+        "id": "38",
+        "name": "mlb",
+        "url": "https://mlb.com"
+    },
+    {
+        "id": "39",
+        "name": "nettruyen",
+        "url": "https://nettruyen.com"
+    },
+    {
+        "id": "40",
+        "name": "reddit",
+        "url": "https://reddit.com"
+    },
+    {
+        "id": "41",
+        "name": "quora",
+        "url": "https://quora.com"
+    },
+    {
+        "id": "42",
+        "name": "duolingo",
+        "url": "https://duolingo.com"
+    },
+    {
+        "id": "43",
+        "name": "udemy",
+        "url": "https://udemy.com"
+    },
+    {
+        "id": "44",
+        "name": "coursera",
+        "url": "https://coursera.org"
+    },
+    {
+        "id": "45",
+        "name": "khanacademy",
+        "url": "https://khanacademy.org"
+    },
+    {
+        "id": "46",
+        "name": "codecademy",
+        "url": "https://codecademy.com"
+    },
+    {
+        "id": "47",
+        "name": "freecodecamp",
+        "url": "https://freecodecamp.org"
+    },
+    {
+        "id": "48",
+        "name": "w3schools",
+        "url": "https://w3schools.com"
+    },
+    {
+        "id": "49",
+        "name": "hackerrank",
+        "url": "https://hackerrank.com"
+    },
+    {
+        "id": "50",
+        "name": "leetcode",
+        "url": "https://leetcode.com"
+    },
+    {
+        "id": "51",
+        "name": "geeksforgeeks",
+        "url": "https://geeksforgeeks.org"
+    },
+    {
+        "id": "52",
+        "name": "hackerearth",
+        "url": "https://hackerearth.com"
+    },
+    {
+        "id": "53",
+        "name": "topcoder",
+        "url": "https://topcoder.com"
+    },
+    {
+        "id": "54",
+        "name": "codeforces",
+        "url": "https://codeforces.com"
+    },
+    {
+        "id": "55",
+        "name": "atcoder",
+        "url": "https://atcoder.jp"
+    },
+    {
+        "id": "56",
+        "name": "spoj",
+        "url": "https://spoj.com"
+    },
+    {
+        "id": "57",
+        "name": "projecteuler",
+        "url": "https://projecteuler.net"
+    },
+    {
+        "id": "58",
+        "name": "codechef",
+        "url": "https://codechef.com"
+    },
+    {
+        "id": "59",
+        "name": "rosalind",
+        "url": "http://rosalind.info"
+    },
+    {
+        "id": "60",
+        "name": "exercism",
+        "url": "https://exercism.io"
+    },
+    {
+        "id": "61",
+        "name": "Expedia",
+        "url": "https://Expedia.com"
+    },
+    {
+        "id": "62",
+        "name": "Merriam-webster",
+        "url": "https://Merriam-webster.com"
+    },
+    {
+        "id": "63",
+        "name": "Dictionary",
+        "url": "https://Dictionary.com"
+    },
+    {
+        "id": "64",
+        "name": "Overstock",
+        "url": "https://Overstock.com"
+    },
+    {
+        "id": "65",
+        "name": "codeforces",
+        "url": "https://codeforces.com"
+    },
+    {
+        "id": "66",
+        "name": "Timviec365",
+        "url": "https://Timviec365.com"
+    },
+    {
+        "id": "67",
+        "name": "atcoder",
+        "url": "https://atcoder.jp"
+    },
+    {
+        "id": "68",
+        "name": "Foodnetwork",
+        "url": "https://Foodnetwork.com"
+    },
+    {
+        "id": "69",
+        "name": "whatsapp",
+        "url": "https://whatsapp.net"
+    },
+    {
+        "id": "70",
+        "name": "codechef",
+        "url": "https://codechef.com"
+    },
+    {
+        "id": "71",
+        "name": "live",
+        "url": "http://live.com"
+    },
+    {
+        "id": "72",
+        "name": "exercism",
+        "url": "https://exercism.io"
+    },
+    {
+        "id": "73",
+        "name": "Vocabulary",
+        "url": "https://Vocabulary.com"
+    },
+    {
+        "id": "74",
+        "name": "Weather",
+        "url": "https://Weather.com"
+    },
+    {
+        "id": "75",
+        "name": "topcvnv",
+        "url": "https://topcvvn.vn"
+    },
+    {
+        "id": "76",
+        "name": "shoppee",
+        "url": "https://shoppee.vn"
+    },
+    {
+        "id": "77",
+        "name": "codeforces",
+        "url": "https://codeforces.com"
+    },
+    {
+        "id": "78",
+        "name": "Glints",
+        "url": "https://Glints.com"
+    },
+    {
+        "id": "79",
+        "name": "atcoder",
+        "url": "https://atcoder.jp"
+    },
+    {
+        "id": "80",
+        "name": "Reference",
+        "url": "https://Reference.com"
+    },
+    {
+        "id": "81",
+        "name": "samsung",
+        "url": "https://samsung.com"
+    },
+    {
+        "id": "82",
+        "name": "codechef",
+        "url": "https://codechef.com"
+    },
+    {
+        "id": "83",
+        "name": "Cambridge",
+        "url": "http://Cambridge.org"
+    },
+    {
+        "id": "84",
+        "name": "exercism",
+        "url": "https://exercism.io"
+    },
+    {
+        "id": "85",
+        "name": "People",
+        "url": "https://People.com"
+    },
+    {
+        "id": "86",
+        "name": "Urbandictionary",
+        "url": "https://Urbandictionary.com"
+    },
+    {
+        "id": "87",
+        "name": "Vietnamwork",
+        "url": "https://Vietnamwork.com"
+    },
+    {
+        "id": "88",
+        "name": "lazada",
+        "url": "https://lazada.vn"
+    },
+    {
+        "id": "89",
+        "name": "codeforces",
+        "url": "https://codeforces.com"
+    },
+    {
+        "id": "90",
+        "name": "office",
+        "url": "https://office.com"
+    },
+    {
+        "id": "91",
+        "name": "atcoder",
+        "url": "https://atcoder.jp"
+    },
+    {
+        "id": "92",
+        "name": "Indeed",
+        "url": "https://Indeed.com"
+    },
+    {
+        "id": "93",
+        "name": "baothanhnien",
+        "url": "https://baothanhnien.vn"
+    },
+    {
+        "id": "94",
+        "name": "codechef",
+        "url": "https://codechef.com"
+    },
+    {
+        "id": "95",
+        "name": "xnxx",
+        "url": "http://xnxx.com"
+    },
+    {
+        "id": "96",
+        "name": "exercism",
+        "url": "https://exercism.io"
+    },
+    {
+        "id": "97",
+        "name": "Foxnews",
+        "url": "https://Foxnews.com"
+    },
+    {
+        "id": "98",
+        "name": "Homedepot",
+        "url": "https://Homedepot.com"
+    },
+    {
+        "id": "99",
+        "name": "indeed",
+        "url": "https://indeed.vn"
+    },
+    {
+        "id": "100",
+        "name": "tiki",
+        "url": "https://tiki.vn"
+    }
+]
+
+
+module.exports = { webData }
